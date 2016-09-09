d9b4c542649e2b6392b329d2fd6bf278cf459e2d
soft <soft@soft-PC>
2016-09-09 10:32:42 +0900

    hallo Java master branch change a code

--- 160906/src/javabook/ch2/HelloWorld.java
+++ 160906/src/javabook/ch2/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
-		System.out.printf("World's End DanceHall 2013041037 ¹Ú°æÈÆ");
+		System.out.printf("World's End Dancehall 2013041037 ¹Ú°æÈÆ");
+		System.out.printf("Earth's End Danchall 2013041037 ¹Ú°æÈÆ");
 	}
 
 }
