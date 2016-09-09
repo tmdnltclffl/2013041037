79432b8031d0d34eac9268b6faf1215e4c85db62
soft <soft@soft-PC>
2016-09-09 10:26:46 +0900

    HelloJava

---
+++ 160906/src/javabook/ch2/HelloWorld.java
@@ -0,0 +1,10 @@
+package javabook.ch2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
+	}
+
+}
