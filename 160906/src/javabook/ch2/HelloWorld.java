3bb3e7653daf7c8fb8c4de10960e0b5021f20572
soft <soft@soft-PC>
2016-09-09 10:30:21 +0900

    Hello Java add print line

--- 160906/src/javabook/ch2/HelloWorld.java
+++ 160906/src/javabook/ch2/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
+		System.out.printf("World's End DanceHall 2013041037 ¹Ú°æÈÆ");
 	}
 
 }
