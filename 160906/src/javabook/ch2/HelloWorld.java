36bb0dce297c2a4e81f7aa2d16655bdd811051b1
soft <soft@soft-PC>
2016-09-13 16:12:17 +0900

    0913 1st

--- 160906/src/javabook/ch2/HelloWorld.java
+++ 160906/src/javabook/ch2/HelloWorld.java
@@ -1,12 +1,22 @@
 package javabook.ch2;
 
+import java.util.Scanner;
+
 public class HelloWorld {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
-		System.out.printf("World's End DanceHall 2013041037 ¹Ú°æÈÆ");
-		System.out.printf("Hellow Dance World 2013041037 ¹Ú°æÈÆ");
-	}
+		System.out.println("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
+		System.out.println("World's End DanceHall 2013041037 ¹Ú°æÈÆ");
+		System.out.println("Hellow Dance World 2013041037 ¹Ú°æÈÆ");
+		Scanner sc = new Scanner(System.in);
+		int number;
+		number = sc.nextInt();    
+		if(number>0)
+			System.out.println("ÀÔ·Â ¹ŞÀº ¼ıÀÚ : " + number);
+		else if(number<=0)
+			System.out.println("¤¸¤µ");
+		}
 
 }
