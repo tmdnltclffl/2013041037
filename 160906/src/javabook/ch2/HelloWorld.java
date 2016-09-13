6463436fae4a862b4df911f892f8902685517a4e
soft <soft@soft-PC>
2016-09-13 16:18:31 +0900

    Scanner

--- 160906/src/javabook/ch2/HelloWorld.java
+++ 160906/src/javabook/ch2/HelloWorld.java
@@ -7,9 +7,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Çï·Î ÀÚ¹Ù 2013041037 ¹Ú°æÈÆ");
-		System.out.println("World's End DanceHall 2013041037 ¹Ú°æÈÆ");
-		System.out.println("Hellow Dance World 2013041037 ¹Ú°æÈÆ");
 		Scanner sc = new Scanner(System.in);
 		int number;
 		number = sc.nextInt();    
