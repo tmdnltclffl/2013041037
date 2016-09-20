118bae4b237c57433a86e345407dc3226e091a8f
soft <soft@soft-PC>
2016-09-20 16:33:56 +0900

    welcome to awt

--- 160906/src/javabook/ch2/day0920.java
+++ 160906/src/javabook/ch2/day0920.java
@@ -1,5 +1,16 @@
 package javabook.ch2;
-
+import java.awt.*;
 public class day0920 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f = new Frame("Hello World");
+		f.setLayout(new FlowLayout());
+		Label label = new Label("Welcome to AWT");
+		
+		f.add(label);
+		f.setSize(200,200);
+		f.setVisible(true);
+	}
 
 }
