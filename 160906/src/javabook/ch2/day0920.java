730652ed54595108415ceb2c74e9434f07e7394d
soft <soft@soft-PC>
2016-09-20 16:43:06 +0900

    login

--- 160906/src/javabook/ch2/day0920.java
+++ 160906/src/javabook/ch2/day0920.java
@@ -1,16 +1,35 @@
 package javabook.ch2;
 import java.awt.*;
-public class day0920 {
 
+
+
+public class day0920 extends Frame{
+	public day0920(){
+		super("AWT 기본예제");
+		setLayout(new BorderLayout());
+		Label t1 = new Label("ID");
+		Label t2 = new Label("Passwd");
+		TextField id = new TextField(10);
+		TextField pwd = new TextField(10);
+		Button btn = new Button("Login");
+		Panel p1 = new Panel();
+		
+		p1.add(t1);
+		p1.add(id);
+		Panel p2 = new Panel();
+		p2.add(t2);
+		p2.add(pwd);
+		p2.add(btn);
+		
+		add(p1, BorderLayout.NORTH);
+		add(p2, BorderLayout.SOUTH);
+		
+		setSize(250,200);
+		setVisible(true);
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Frame f = new Frame("Hello World");
-		f.setLayout(new FlowLayout());
-		Label label = new Label("Welcome to AWT");
-		
-		f.add(label);
-		f.setSize(200,200);
-		f.setVisible(true);
+		day0920 app= new day0920();
 	}
-
 }
