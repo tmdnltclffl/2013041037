d7ac1cbbd06a1cb45931a35eace817f41afba1f0
soft <soft@soft-PC>
2016-09-23 11:46:51 +0900

    0923

---
+++ 160906/src/javabook/ch2/day0923.java
@@ -0,0 +1,75 @@
+package javabook.ch2;
+
+public class day0923 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		int numOfApples = 123; // 사과의 개수
+		int sizeOfBucket = 10; // 바구니의 크기(바구니에 담을 수 있는 사과의 개수)
+		int numOfBucket = ( numOfApples+numOfApples%sizeOfBucket>0?1:0 ); // 모든 사과를 담는데 필요한 바구니의 수
+		System.out.println("필요한 바구니의 수 :"+numOfBucket);*/
+		
+//		int num = 10;
+//		System.out.println( num==0?0:num>0?"양수":"음수" );
+		
+		
+//		int num = 456;
+//		System.out.println(num/100*100);
+		
+		
+//		int num = 333;
+//		System.out.println( num/10*10+1 );
+		
+//		int num = 24;
+//		System.out.println( (num%10>0?10:0)+num/10*10 );
+		
+//		int fahrenheit = 100;
+//		float celcius = (float) ((int)(5/9f* (fahrenheit - 32)*100)/100f+(((5/9f* (fahrenheit - 32))*1000%10>4?0.01:0)));
+//		System.out.println("Fahrenheit:"+fahrenheit);
+//		System.out.println("Celcius:"+celcius);
+		
+//		byte a = 10;
+//		byte b = 20;
+//		byte c = (byte) (a + b);
+//		char ch = 'A';
+//		ch = (char) (ch + 2);
+//		float f = 3 / 2f;
+//		long l = 3000l * 3000 * 3000;
+//		float f2 = 0.1f;
+//		double d = 0.1f;
+//		boolean result = d==f2;
+//		System.out.println("c="+c);
+//		System.out.println("ch="+ch);
+//		System.out.println("f="+f);
+//		System.out.println("l="+l);
+//		System.out.println("result="+result);
+		
+//		char ch = 'z';
+//		boolean b = (0<=ch&&ch<=9||'a'<=ch&&ch<='z'||'A'<=ch&&ch<='Z');
+//		System.out.println(b);
+		
+//		char ch = 'A';
+//		char lowerCase = (char) (( ch<96 ) ? ( ch+32 ) : ch);
+//		System.out.println("ch:"+ch);
+//		System.out.println("ch to lowerCase:"+lowerCase);
+		
+//		int i=0; 
+//		int j=0;
+//		while(i<=10) {
+//			while(j<=i)
+//			{
+//				System.out.print("*");
+//				j++;
+//			}
+//			System.out.println();
+//			j=0;
+//			i++;
+//		}
+		
+//		int value = (int) ( Math.random()*10%6+1 );
+//		System.out.println("value:"+value);
+
+
+	}
+	
+}
