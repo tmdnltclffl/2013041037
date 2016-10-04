f06b013440460fbc3a1d9dd6acaca5336e658718
soft <soft@soft-PC>
2016-10-04 17:43:04 +0900

    161004

--- 160906/src/javabook/ch2/day161004.java
+++ 160906/src/javabook/ch2/day161004.java
@@ -6,10 +6,10 @@ public class day161004 {
 	int num2 = 10;
 	
 	
-/*	public day161004(){
+	public day161004(){
 		System.out.println("按眉 积己 己傍: 秋肺岿靛");
 		System.out.println("按眉 积己 肚 己傍");
-	}*/
+	}
 	
 	public static void main(String[] args){
 		day161004 exam = new day161004();
