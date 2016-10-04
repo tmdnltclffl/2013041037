032b0333d4b6e55f25c49cbd66d61a816fba583d
soft <soft@soft-PC>
2016-10-04 16:58:28 +0900

    160930

--- 160906/src/javabook/ch2/day160930.java
+++ 160906/src/javabook/ch2/day160930.java
@@ -161,7 +161,28 @@ public class day160930 {
 			System.out.println("src:"+src);
 			System.out.println("result:"+result);*/
 		
-		
+		int[][] score = {
+				{100, 100, 100}
+				, {20, 20, 20}
+				, {30, 30, 30}
+				, {40, 40, 40}
+				, {50, 50, 50}
+				};
+				int[][] result = new int[score.length+1][score[0].length+1];
+				for(int i=0; i < score.length;i++) {
+				for(int j=0; j < score[i].length;j++) {
+					int sum=0;
+					sum += score[i][j];
+					if(j==score.length){
+						result[i][j+1]=sum;
+					}
+				}
+				}
+				for(int i=0; i < result.length;i++) {
+				for(int j=0; j < result[i].length;j++) {
+				System.out.printf("%4d",result[i][j]);
+				} System.out.println();
+				}
 		
 			
 	
