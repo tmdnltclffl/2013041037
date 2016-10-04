e2f6f8fc053542eec134596acd39d4aac26ea91c
soft <soft@soft-PC>
2016-10-04 17:05:21 +0900

    161004

---
+++ 160906/src/javabook/ch2/day161004.java
@@ -0,0 +1,13 @@
+package javabook.ch2;
+
+public class day161004 {
+	public day161004(){
+		// TODO Auto-generated method stub
+		System.out.println("按眉 积己 己傍: 秋肺岿靛");
+	}
+	
+	public static void main(String[] args){
+		day161004 exam = new day161004();
+	}
+
+}
