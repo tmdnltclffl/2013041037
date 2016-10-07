37844c482c6669eb8d200deb20f1510369e681c3
soft <soft@soft-PC>
2016-10-07 10:43:16 +0900

    day161007 10:43

---
+++ 160906/src/javabook/ch2/day161007.java
@@ -0,0 +1,62 @@
+package javabook.ch2;
+
+public class day161007 {
+
+	
+	//추상클레스
+/*	abstract class MyAbstractClass{
+		int num1,num2;
+		int result;
+	
+		void calc(){
+			result=num1+num2;
+		}
+	
+		abstract int getResult();
+		
+	}
+	
+	class MyClass extends MyAbstractClass{
+		int getResult(){
+			return result;
+		}
+	}
+*/
+	
+	
+	
+	//인터페이스
+/*	interface MyInterface{
+		public static final int num1= 1000;
+		public abstract int getResult();
+	}
+	class MyClass implements MyInterface{
+		public int getResult(){
+			return num1 + 300;
+		}
+	}*/
+	
+	
+	//오버라이딩
+	/*@Override
+	public String toString(){
+		return "오버라이딩 예제입니다.";
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		day161007 exam = new day161007();
+		day161007 exam2 = new day161007();
+
+		System.out.println(exam.toString());
+		System.out.println(exam2.toString());
+	}
+*/
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
