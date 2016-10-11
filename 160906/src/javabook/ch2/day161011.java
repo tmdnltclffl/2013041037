cf72cdc22089b2d20aad7cb98e6aaa92f44f588a
soft <soft@soft-PC>
2016-10-11 17:24:04 +0900

    161011 5:24

--- 160906/src/javabook/ch2/day161011.java
+++ 160906/src/javabook/ch2/day161011.java
@@ -1,21 +1,37 @@
 package javabook.ch2;
 
 public class day161011 {
-	String msg;
+	private String msg;
+	static int count = 0;
+	static String prefix = "";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	/*	System클래스.out객체.println메소드("hello world");*/
 		
 		day161011 hello1 = new day161011();
 		hello1.print();
+		
 		day161011 hello2 = new day161011("My Hello World!!");
 		hello2.print();
+		
 		day161011 hello3 = new day161011("박경훈");
 		hello3.print();
+		
 		day161011 hello4 = new day161011("Hello",2);
 		hello4.print();
+		
 		hello2.setMsg("반갑습니다!!");
 		System.out.println(hello2.getMsg());
+		
+		day161011.prefix = "Greetings: ";
+		hello2.print();
+		hello3.print();
+		
+		day161011.prefix = "인사말: ";
+		hello2.print();
+		hello3.print();
+				
 	}
 	public day161011(){
 		msg="Hello World !!";
@@ -30,16 +46,17 @@ public class day161011 {
 			this.msg=msg+", 안녕하세요?";
 	}
 	public void print(){
-		System.out.println(msg);
+		//System.out.println(msg);
+		count++;
+		System.out.println("["+count+"]"+prefix+msg);
 	}
-	
-	public String getMsg(){
+	public String getMsg() {
 		return msg;
 	}
-	public void setMsg(String msg){
-		this.msg=msg;
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
-	
-	
+
+		
 
 }
