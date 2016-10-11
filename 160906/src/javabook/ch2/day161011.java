f8fd84d9678f9315c3c132b66c4427fa91dbfea0
soft <soft@soft-PC>
2016-10-11 17:09:35 +0900

    161011

--- 160906/src/javabook/ch2/day161011.java
+++ 160906/src/javabook/ch2/day161011.java
@@ -8,11 +8,38 @@ public class day161011 {
 		
 		day161011 hello1 = new day161011();
 		hello1.print();
+		day161011 hello2 = new day161011("My Hello World!!");
+		hello2.print();
+		day161011 hello3 = new day161011("¹Ú°æÈÆ");
+		hello3.print();
+		day161011 hello4 = new day161011("Hello",2);
+		hello4.print();
+		hello2.setMsg("¹Ý°©½À´Ï´Ù!!");
+		System.out.println(hello2.getMsg());
 	}
 	public day161011(){
 		msg="Hello World !!";
 	}
+	public day161011(String msg){
+		this.msg=msg;
+	}
+	public day161011(String msg,int option){
+		if(option==1)
+			this.msg=msg;
+		else if(option==2)
+			this.msg=msg+", ¾È³çÇÏ¼¼¿ä?";
+	}
 	public void print(){
 		System.out.println(msg);
 	}
+	
+	public String getMsg(){
+		return msg;
+	}
+	public void setMsg(String msg){
+		this.msg=msg;
+	}
+	
+	
+
 }
