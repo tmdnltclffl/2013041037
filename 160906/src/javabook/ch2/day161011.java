518a7516af5c3d8aa7630264e761abab6e8acc46
soft <soft@soft-PC>
2016-10-11 16:27:12 +0900

    161011

--- 160906/src/javabook/ch2/day161011.java
+++ 160906/src/javabook/ch2/day161011.java
@@ -1,12 +1,18 @@
 package javabook.ch2;
 
 public class day161011 {
-
+	String msg;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+	/*	System클래스.out객체.println메소드("hello world");*/
+		
 		day161011 hello1 = new day161011();
-
+		hello1.print();
 	}
-
+	public day161011(){
+		msg="Hello World !!";
+	}
+	public void print(){
+		System.out.println(msg);
+	}
 }
