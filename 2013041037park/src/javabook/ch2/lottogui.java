d99241ec420165290f03a720d2a42b043adcb074
soft <soft@soft-PC>
2016-11-01 17:42:04 +0900

    LOTTO

--- 2013041037park/src/javabook/ch2/lottogui.java
+++ 2013041037park/src/javabook/ch2/lottogui.java
@@ -10,17 +10,17 @@ public class lottogui extends JFrame implements ActionListener {
 	
 	public lottogui(int[] arr){
 		super("Hello World");
-		getContentPane().setLayout(new FlowLayout());
+		
 		Panel p1 = new Panel();
 		Panel p2 = new Panel();
 		p2.add(btn);
 		JLabel label = new JLabel(Arrays.toString(arr));
 		p1.add(label);
 		btn.addActionListener(this);
-		add(p1,BorderLayout.SOUTH);
-		add(p2,BorderLayout.NORTH);
+		add(p1,BorderLayout.NORTH);
+		add(p2,BorderLayout.SOUTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(300,150);
+		setSize(300,100);
 		setVisible(true);
 	}
 
